e4c454211a16274df54faef76b60ab31397a6ec7
Brad Simantel <brad@ottter.com>
2025-03-02 14:25:49 -0800

    Remove nvim config

--- .config/nvim/ftplugin/javascript.lua
+++
@@ -1,6 +0,0 @@
--- https://f1sty.github.io/neovim/nvim/lsp/language-server/config/dotfiles/completion/2023/09/10/simple-nvim-lsp-setup.html
-vim.lsp.start({
-  name = 'tsserver',
-  cmd = {'tsserver'},
-  root_dir = vim.fs.dirname(vim.fs.find({ 'package.json' }, { upward = true })[1]),
-})
